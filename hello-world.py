75df8edc1e07178051a449e71ff9a21717c01f92
Mateusz <smetmateusz@gmail.com>
2022-12-02 14:34:35 +0100

    Breaking master merge
    Not really tho

--- hello-world.py
+++ hello-world.py
@@ -2,4 +2,7 @@ print("Witaj swiecie okrutny")
 x = 1 + 1
 
 print("Dodalem dla Ciebie po cichu 2 liczby", x)
+y = input("Daj mnie jeszcze jedna liczbe, dodam ja do poprzedniej :)")
+z = x + y
+print("Dziena wariacie!")
 
