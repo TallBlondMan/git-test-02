a671b73148d4bcde1ea0398da55ca68df865771b
Matejusz <smetmateusz@gmail.com>
2023-12-18 16:46:12 +0100

    Fixed the code

--- hello-world.py
+++ hello-world.py
@@ -2,7 +2,7 @@ print("Hello oh World")
 x = 1 + 1
 
 print("I added some numbers for you here, have a look ", x)
-y = input("Now give me another number, I'ma gonna add it to the one above")
-z = x + y 
+y = input("Now give me another number, I'ma gonna add it to the one above: ")
+z = x + int(y) 
 print("Nice, Thanks")
 
